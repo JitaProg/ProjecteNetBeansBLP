e98e440fa02ef071985ddba5db10bc351ecd83d0
JitaProg <borjablp2@gmail.com>
2025-12-03 13:16:07 +0100

    Comentari que identifica el primer commit - Primer commit.

---
+++ src/main/java/com/mycompany/projectenetbeansblp/ProjecteNetBeansBLP.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projectenetbeansblp;
+
+/**
+ *
+ * @author borja
+ */
+public class ProjecteNetBeansBLP {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
